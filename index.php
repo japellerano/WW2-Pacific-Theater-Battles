d7a291b761db5a04afad739d4517646788df42d2
James Pellerano <james@japellerano.com>
2011-12-13 15:12:29 -0500

    Adding of Events
    Added events and styled the description text. Also added the image for the second event, when America declared war on the Axis Powers.
    
    Signed-off-by: James Pellerano <james@japellerano.com>

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
           <ul id="dates">
             <li><a href="#07DEC41">07 December 1941</a></li>
             <li><a href="#11DEC41">11 December 1941</a></li>
+            <li><a href="#04MAY42">04 May 1942</a></li>
+            <li><a href="#03JUN42">03 June 1942</a></li>
+            <li><a href="#04JUN42">04 June 1942</a></li>
+            <li><a href="#06AUG45">06 August 1945</a></li>
+            <li><a href="#09AUG45">09 August 1945</a></li>
           </ul>
           
           <ul id="issues">
@@ -86,9 +91,73 @@
                 </div>
             </li>
             <li id="11DEC41">
-                <h1 class="title">United States Declares War on Axis Powers</h1>
-                <p class="text"></p>
+                <div class="left">
+                    <h1 class="title">United States Declares War on Axis Powers</h1>
+                    <p class="descript">
+                        The Axis powers declare war on the United States. The United States declares war on the Axis powers. The Axis powers are Italy, Germany and Japan.
+                    </p>
+                </div>
+                <div class="map">
+                    <img src="images/declares_war.jpg" />
+                </div>
             </li>
+            
+            <li id="04MAY42">
+                <div class="left">
+                    <h1 class="title">Battle of the Coral Sea</h1>
+                    <p class="descript">
+                        04 May 1942 - 08 May 1942.
+                    </p>
+                </div>
+                <div class="map">
+                    <img src="http://maps.googleapis.com/maps/api/staticmap?center=-12.6,156.7&zoom=05&size=300x250&maptype=terrain&sensor=false" />
+                </div>
+            </li>
+            
+            <li id="03JUN42">
+                <div class="left">
+                    <h1 class="title">Aleutian Island Campaign</h1>
+                    <p class="descript">
+                        03 June 1942 - 15 August 1943
+                    </p>
+                </div>
+                <div class="map">
+                    <img src="http://maps.googleapis.com/maps/api/staticmap?center=51.67,-177.3&zoom=04&size=300x250&maptype=terrain&sensor=false" />
+                </div>
+            </li>
+            
+            <li id="04JUN42">
+                <div class="left">
+                    <h1 class="title">Battle of Midway</h1>
+                    <p class="descript">
+                        04 June 1942 - 07 June 1942
+                    </p>
+                </div>
+                <div class="map">
+                    <img src="http://maps.googleapis.com/maps/api/staticmap?center=28.233,-177.352&zoom=11&size=300x250&maptype=terrain&sensor=false" />
+                </div>
+            </li>
+            
+            <li id="06AUG45">
+                <div class="left">
+                    <h1 class="title">United States Drops the First Atomic Bomb on Hiroshima</h1>
+                    <p class="descript">
+                    </p>
+                </div>
+                <div class="map">
+                </div>
+            </li>
+            
+            <li id="09AUG45">
+                <div class="left">
+                    <h1 class="title">United States Drops the Second Atomic Bomb on Nagasaki</h1>
+                    <p class="descript">
+                    </p>
+                </div>
+                <div class="map">
+                </div>
+            </li>
+            
           </ul>
           
           <a href="#" id="next">+</a>
