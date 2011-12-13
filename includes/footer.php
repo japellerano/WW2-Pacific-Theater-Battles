7dd2ed6e84205fcc7dc35fc681582aed7f739b49
James Pellerano <james@japellerano.com>
2011-12-12 23:55:52 -0500

    basic styling of site and timeline, using text-indent instead of margin-left. margin-left pushes the next "slide" over too far

--- includes/footer.php
+++ includes/footer.php
@@ -1,7 +1,7 @@
          <div class="clear"></div>
          <footer>
             <p class="foot">
-               Built using <a href="http://www.kalyani.com/2010/10/subway-map-visualization-jquery-plugin/">Subway Map Visualization jQuery Plugin</a> and <a href="http://php.net">PHP</a>.
+               Built using <a href="http://www.kalyani.com/2010/10/subway-map-visualization-jquery-plugin/">Subway Map Visualization jQuery Plugin</a>, <a href="http://www.csslab.cl/2011/08/18/jquery-timelinr/">jQuery Timelinr</a> and <a href="http://php.net">PHP</a>.
             </p>
             <p class="foot">
                Copyright &copy; <?php echo date('Y'); ?> <a href="http://japellerano.com/">James Pellerano</a>. All Rights Reserved.
