075e3ff6ac13324b32cacc60aea3b2f2cd7f800d
James Pellerano <james@japellerano.com>
2011-12-14 00:56:28 -0500

    Deployment Setup
    Signed-off-by: James Pellerano <james@japellerano.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,20 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :user, 'p10r8267'
+set :domain, '66.84.0.145'
+set :application, 'class.japellerano.com'
+set :current_dir, '/class'
+ssh_options[:keys] = %w(/Users/james/.ssh/id_dsa)
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :repository, 'git@github.com:japellerano/WW2-Pacific-Theater-Battles.git'
+set :deploy_to, '~/subdomains/'
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true
+set :git_shallow_clone, 1
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+role :web, "#{application}"
+role :app, "#{application}"
+role :db, "#{application}"
+
