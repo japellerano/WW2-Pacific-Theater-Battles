01e568c34f8fda341502d1d4cfb8e8ac6baf22ab
James Pellerano <james@japellerano.com>
2011-12-14 04:20:32 -0500

    Updates
    Updates to About content. Styles for About page and fixed the size of the selected date on the home page.
    
    Signed-off-by: James Pellerano <james@japellerano.com>

--- about.php
+++ about.php
@@ -42,18 +42,39 @@
         </div>
         
         <h1 class="section">About The Author</h1>
-        <p class="text">
-        
+        <p class="about">
+            My name is James Addison Pellerano, I am a student at Northeastern University, majoring in Digital Art. Front-End Web Developer specializing in WordPress theme development and front-end coding. Check out my projects on GitHub: <a href="http://github.com/japellerano/">http://github.com/japellerano/</a>.
         </p>
     </div>
     <div id="content-right">
-        <div class="twitterfeed">
-            <img src="images/twitter_bird.png" width="99" height="75" alt="Twitter" />
-            <h3>Follow <a href="http://twitter.com/frontendjames">James Pellerano on Twitter</a></h3>
-            
-            <div id="tweet">
-            </div>
-        </div>
+        <script src="http://widgets.twimg.com/j/2/widget.js"></script>
+        <script>
+        new TWTR.Widget({
+          version: 2,
+          type: 'profile',
+          rpp: 4,
+          interval: 30000,
+          width: 250,
+          height: 300,
+          theme: {
+            shell: {
+              background: '#333333',
+              color: '#ffffff'
+            },
+            tweets: {
+              background: '#000000',
+              color: '#ffffff',
+              links: '#4aed05'
+            }
+          },
+          features: {
+            scrollbar: false,
+            loop: false,
+            live: false,
+            behavior: 'all'
+          }
+        }).render().setUser('frontendjames').start();
+        </script>
     </div>
 </div>
 
