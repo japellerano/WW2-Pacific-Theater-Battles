8a698152bae16e4f842c0d3a12fc628a50fc726c
James Pellerano <james@japellerano.com>
2011-12-14 03:53:14 -0500

    Adding Twitter and about content
    Signed-off-by: James Pellerano <james@japellerano.com>

--- about.php
+++ about.php
@@ -1,7 +1,61 @@
 <?php include('includes/header.php'); ?>
 
-<div id="content">
-    <h1 class="section"></h1>
+<div id="container">
+    <div id="content-left">
+        <h1 class="section">About the Project</h1>
+        <div class="about">
+            This is the third and final project for ARTG 3352 - Interactive Design. The project focuses on time and information which has time relevancy.
+        </div>
+        
+        <h1 class="section">The Technologies Used</h1>
+        <div class="about">
+            <article>
+                <h3 class="tech-header">Google Static Map API</h3>
+                <p>
+                    <a href="http://code.google.com/apis/maps/documentation/staticmaps/">http://code.google.com/apis/maps/documentation/staticmaps/</a>
+                </p>
+            </article>
+            <article>
+                <h3 class="tech-header">jQuery Subway Map Visualization Plugin</h3>
+                <p>
+                    <a href="http://www.kalyani.com/2010/10/subway-map-visualization-jquery-plugin/">http://www.kalyani.com/2010/10/subway-map-visualization-jquery-plugin/</a>
+                </p>
+            </article>
+            <article>
+                <h3 class="tech-header">jQuery Timelinr</h3>
+                <p>
+                    <a href="http://www.csslab.cl/2011/08/18/jquery-timelinr/">http://www.csslab.cl/2011/08/18/jquery-timelinr/</a>
+                </p>
+            </article>
+            <article>
+                <h3 class="tech-header">Git Version Control</h3>
+                <p>
+                    <a href="http://git-scm.com/">http://git-scm.com/</a>
+                </p>
+            </article>
+            <article>
+                <h3 class="tech-header">GitHub - Git Code Repository</h3>
+                <p>
+                    <a href="http://github.com">http://github.com</a>
+                </p>
+            </article>
+        </div>
+        
+        <h1 class="section">About The Author</h1>
+        <p class="text">
+        
+        </p>
+    </div>
+    <div id="content-right">
+        <div class="twitterfeed">
+            <img src="images/twitter_bird.png" width="99" height="75" alt="Twitter" />
+            <h3>Follow <a href="http://twitter.com/frontendjames">James Pellerano on Twitter</a></h3>
+            
+            <div id="tweet">
+            </div>
+        </div>
+    </div>
 </div>
+
 
 <?php include('includes/footer.php'); ?>
